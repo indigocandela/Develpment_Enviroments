c2a89e4527b9ceb4fcb11e56260f61e6d381fdbc
iñigo <indigocandela98@gmail.com>
2022-03-07 13:38:21 +0100

    "3"

---
+++ server.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const bodyParser=require("body-parser");
+const app=express();
+
+require("dotenv-flow").config();
+
+//route
+app.get("/api/welcome",(req,res)=>{
+    res.status(200).send({message: "Welcome to the MEN Restful API"});
+})
+
+const PORT=process.env.PORT || 4000;
+app.listen(PORT,function(){
+    console.log("Server is runnig on port: " +PORT);
+
+})
+module.exports=app;
