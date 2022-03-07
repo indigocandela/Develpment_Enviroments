b6dbde01d788be1d4ec9b48e15da40dd441ea732
iñigo <indigocandela98@gmail.com>
2022-03-07 17:43:19 +0100

    read

--- routes/duck.js
+++ routes/duck.js
@@ -7,10 +7,42 @@ const duck= require("../models/duck");
 
 router.post("/",(req,res)=>{
     data=req.body;
-    
+
     duck.insertMany(data)
     .then(data=>{res.send(data);})
     .catch(err=>{res.status(500).send({message:err.message});})
 });
+
+//See all ducks
+
+router.get("/",(req,res)=>{
+    data=req.body;
+    
+    duck.find()
+    .then(data=>{res.send(data);})
+    .catch(err=>{res.status(500).send({message:err.message});})
+});
+
+//See all  ducks in Stock
+
+router.get("/instock",(req,res)=>{
+    data=req.body;
+    
+    duck.find({inStock: true})
+    .then(data=>{res.send(data);})
+    .catch(err=>{res.status(500).send({message:err.message});})
+});
+
+//See an especific duck
+
+router.get("/:id",(req,res)=>{
+    data=req.body;
+    
+    duck.findById(req.params.id)
+    .then(data=>{res.send(data);})
+    .catch(err=>{res.status(500).send({message:err.message});})
+});
+
+
 
 module.exports=router;
