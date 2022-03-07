a50b6d56361ffaa78af95d2e8d45c8ee38451c51
iñigo <indigocandela98@gmail.com>
2022-03-07 18:08:44 +0100

    Update and delete

--- routes/duck.js
+++ routes/duck.js
@@ -43,6 +43,46 @@ router.get("/:id",(req,res)=>{
     .catch(err=>{res.status(500).send({message:err.message});})
 });
 
+//Update an especific duck
+
+router.put("/:id",(req,res)=>{
+    const id=req.params.id;
+
+    
+    duck.findByIdAndUpdate(id,req.body)
+    .then(data=>{
+    if(!data){
+        res.status(404).send({message : "Cannot update duck with id= " +id+". Maybe the duck was not found!"})
+    }
+    else{
+        res.send({message: "Duck was succesesfully updated."})
+
+    }
+    })
+    .catch(err=>{res.status(500).send({message:"Cannot update duck with id= " +id});
+    })
+});
+
+//Delete an especific duck
+
+router.delete("/:id",(req,res)=>{
+    const id=req.params.id;
+
+    
+    duck.findByIdAndDelete(id)
+    .then(data=>{
+    if(!data){
+        res.status(404).send({message : "Cannot delete duck with id= " +id+". Maybe the duck was not found!"})
+    }
+    else{
+        res.send({message: "Duck was succesesfully deleted."})
+
+    }
+    })
+    .catch(err=>{res.status(500).send({message:"Cannot update duck with id= " +id});
+    })
+});
+
 
 
 module.exports=router;
